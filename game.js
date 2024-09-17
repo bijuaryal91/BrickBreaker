e96d0e2ad90fad7fee5fa3f9a19a035f58c525a4
bijuaryal17@gmail.com <bijuaryal17@gmail.com>
2024-09-18 00:47:10 +0545

    Add brick creation and collision detection

--- game.js
+++ game.js
@@ -57,9 +57,93 @@ function drawBall(ball) {
   ctx.closePath();
 }
 
+// Control Variables
+let rightPressed = false;
+let leftPressed = false;
+
+// Brick Properties
+let brickRowCount = 10;
+let brickColumnCount = Math.floor((canvas.width + 10) / (70 + 10));
+let brickWidth = 70;
+let brickHeight = 15;
+let brickPadding = 10;
+let brickOffsetTop = 55;
+let brickOffsetLeft = 10;
+
+let bricks = [];
+// Creating bricks
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+let score = 0;
+let lives = 3; //Total Lives Available
+
+// Function to draw bricks
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status === 1) {
+        let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#FF6347";
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "#CD5C5C";
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+// Function for collision detection
+function collisionDetection() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      let b = bricks[c][r];
+      if (b.status === 1) {
+        balls.forEach((ball) => {
+          // Check for collision with the brick
+          if (
+            ball.x + ballRadius > b.x &&
+            ball.x - ballRadius < b.x + brickWidth &&
+            ball.y + ballRadius > b.y &&
+            ball.y - ballRadius < b.y + brickHeight
+          ) {
+            ball.dy = -ball.dy; // Reverse ball direction
+            b.status = 0; // Mark brick as broken
+            score++;
+
+            // Todo: Randomly drop a power-up
+
+            // check if all bricks are broken
+            const allBricksBroken = bricks.every((column) => {
+              column.every((brick) => (brick.status = 0));
+            });
+            if(allBricksBroken)
+            {
+                alert("YOU WIN! CONGRATULATIONS!");
+                document.location.reload();
+            }
+          }
+        });
+      }
+    }
+  }
+}
+
 // Gameloop
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   balls.forEach(drawBall);
   drawPaddle();
 
